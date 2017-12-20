2e7a8957573c7130cf2cd73c4d3c22e42681b700
Kamil Kuryś <kamil.kurys@intive.com>
2017-12-20 10:27:23 +0100

    day 2 solved

---
+++ Logic/ChecksumCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic
+{
+    public class ChecksumCalculator
+    {
+        public int[][] LoadMatrixFromFile(string fileName)
+        {
+            string[] rows = System.IO.File.ReadAllText(fileName).Split('\n');
+            int[][] matrix = new int[rows.Length][];
+            for (int i = 0; i < rows.Length; i++)
+            {
+                var row = rows[i].Split('\t');
+                matrix[i] = new int[row.Length];
+                for (int j = 0; j < row.Length; j++)
+                {
+                    matrix[i][j] = int.Parse(row[j].Trim());
+                }
+            }
+            return matrix;
+        }
+        public int CalculateChecksum(int [][] matrix)
+        {
+            int checksum = 0;
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                checksum += CalculateRowDiff(matrix[i]);
+            }
+            return checksum;
+        }
+        private int CalculateRowDiff(int[] row)
+        {
+            return row.Max() - row.Min();
+        }
+    }
+}
---
+++ LogicTests/ChecksumCalculatorTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+
+namespace LogicTests
+{
+    /// <summary>
+    /// Summary description for ChecksumCalculatorTests
+    /// </summary>
+    [TestClass]
+    public class ChecksumCalculatorTests
+    {
+        private ChecksumCalculator _calculator;
+        public ChecksumCalculatorTests()
+        {
+            _calculator = new ChecksumCalculator();
+        }
+
+
+
+        [TestMethod]
+        public void ChecksumCalculator_returns_0_for_equal_numbers()
+        {
+            var example = new int[3][] {
+                new int[]{ 3, 3, 3 },
+                new int[]{ 3, 3, 3 },
+                new int[]{ 3, 3, 3 }
+            };
+
+            var result = _calculator.CalculateChecksum(example);
+
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void ChecksumCalculator_returns_row_diff_between_smallest_and_largest_number()
+        {
+            var example = new int[1][] {
+                new int[]{ 0, 3, 8},
+            };
+
+            var result = _calculator.CalculateChecksum(example);
+
+            Assert.AreEqual(8, result);
+        }
+        [TestMethod]
+        public void ChecksumCalculator_returns_sum_of_row_diffs_between_smallest_and_largest_number_in_each_row()
+        {
+            var example = new int[2][] {
+                new int[]{ 0, 3, 8},
+                new int[]{ 5, 3, 8},
+            };
+
+            var result = _calculator.CalculateChecksum(example);
+
+            Assert.AreEqual(13, result);
+        }
+    }
+}
